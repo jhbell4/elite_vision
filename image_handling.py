3eb56dfdeb1222e0622ac428ea5793b3f7ac5183
jhbell4 <jhbell@mit.edu>
2022-08-17 11:28:38 +0200

    Implemented image plotting
    Created image plotting function and plotted images

---
+++ image_handling.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt # for plotting
+
+def imshow(img):
+  ''' function to show image '''
+  img = img / 2 + 0.5 # unnormalize
+  npimg = img.numpy() # convert to numpy objects
+  plt.imshow(np.transpose(npimg, (1, 2, 0)))
+  plt.show()
--- pytorch_implementation_1.py
+++ pytorch_implementation_1.py
@@ -10,25 +10,45 @@ import torch.nn as nn # basic building block for neural neteorks
 import torch.nn.functional as F # import convolution functions like Relu
 import torch.optim as optim # optimzer
 
-# define directory
-dirname = os.path.dirname(__file__)
-data_directory = os.path.join(dirname, 'vision_dataset\\vision_dataset')
-train_directory = os.path.join(data_directory, 'train')
-test_directory = os.path.join(data_directory, 'test')
+from image_handling import imshow
 
-# python image library of range [0, 1] 
-# transform them to tensors of normalized range[-1, 1]
+def main():
+    # define directory
+    dirname = os.path.dirname(__file__)
+    data_directory = os.path.join(dirname, 'vision_dataset\\vision_dataset')
+    train_directory = os.path.join(data_directory, 'train')
+    test_directory = os.path.join(data_directory, 'test')
 
-transform = transforms.Compose( # composing several transforms together
-    [transforms.ToTensor(), # to tensor object
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]) # mean = 0.5, std = 0.5
+    # python image library of range [0, 1] 
+    # transform them to tensors of normalized range[-1, 1]
 
-# set batch_size
-batch_size = 4
+    transform = transforms.Compose( # composing several transforms together
+        [transforms.ToTensor(), # to tensor object
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]) # mean = 0.5, std = 0.5
 
-# set number of workers
-num_workers = 2
+    # set batch_size
+    batch_size = 4
 
-# load train data
-trainset = torchvision.datasets.ImageFolder(root=train_directory, transform=transform)
-testset = torchvision.datasets.ImageFolder(root=test_directory, transform=transform)
+    # set number of workers
+    num_workers = 2
+
+    # load train data
+    trainset = torchvision.datasets.ImageFolder(root=train_directory, transform=transform)
+    testset = torchvision.datasets.ImageFolder(root=test_directory, transform=transform)
+
+    classes = trainset.classes
+    print(classes)
+
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
+
+    imshow(torchvision.utils.make_grid(images))
+
+    # print the class of the image
+    print(' '.join('%s' % classes[labels[j]] for j in range(batch_size)))
+
+if __name__ == '__main__':
+    main()
